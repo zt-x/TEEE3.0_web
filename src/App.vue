4bcb3be8a77a09afd411bed604aa6f9d43bf0cef
许正韬 <1753059613@qq.com>
2023-01-19 14:47:54 +0800

    init 2

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view style="overflow-x: auto" />
-    </v-main>
-  </v-app>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "App",
-  data: () => ({
-    //
-  }),
-};
-</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-<style scoped>
-.v-main__wrap{
-	overflow: auto;
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,28 +4,14 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-	state: {
-		userInfo: '',
-		pageName:'',
-  	},
-	getters: {
-		getUserInfo: state => {
-			return state.userInfo
-		},
-		getPageName: state => {
-			return state.pageName
-		},
-	},
-	mutations: {
-		updateUserInfo(state, uf) {
-			state.userInfo = uf;
-		},
-		updatePageName(state, uf) {
-			state.pageName = uf;
-		},
-  	},
-	actions: {
-	},
-	modules: {
-	}
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
 })
